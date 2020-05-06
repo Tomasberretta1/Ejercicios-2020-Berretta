7cfb3738ecf31121c413bf5ac8cdf29aadb6ddfd
Tomasberretta1 <tomi.berretta@gmail.com>
2020-05-05 22:15:43 -0300

    Se realizaron ejercicios de JS (Hola mundo, Conversor de unidades, Documentacion de codigo, Grados a Radianes)

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,11 +1,53 @@
 /**
- * Created by Agus on 6/5/2017.
- */
-
-/**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
+ * Conversor de unidades, de metros, pulgadas, pies y yardas.
+ * @method cambiarUnidades
+ * @param {string} id - El id de los inputs de metros, pulgadas, pies o yardas
+ * @param {numer} valor - El valor de los inputs de metros, pulgadas, pies o yardas
  * @return Valor que retorna
  */
+
+function CambiarUnidades(id, valor) {
+    if (isNaN(valor)) {
+        alert("Se ingreso un valor invalido " + id);
+        document.lasUnidades.unid_metro.value = "";
+        document.lasUnidades.unid_pulgada.value = "";
+        document.lasUnidades.unid_pie.value = "";
+        document.lasUnidades.unid_yarda.value = "";
+
+    } else if (id == "metro") {
+        document.lasUnidades.unid_pulgada.value = 39.3701 * valor;
+        document.lasUnidades.unid_pie.value = 3.28084 * valor;
+        document.lasUnidades.unid_yarda.value = 1.09361 * valor;
+
+    } else if (id == "pulgada") {
+        document.lasUnidades.unid_metro.value = 0.0254 * valor;
+        document.lasUnidades.unid_pie.value = 0.0833333 * valor;
+        document.lasUnidades.unid_yarda.value = 0.027778 * valor;
+
+    } else if (id == "pie") {
+        document.lasUnidades.unid_metro.value = 0.3048 * valor;
+        document.lasUnidades.unid_pulgada.value = 12 * valor;
+        document.lasUnidades.unid_yarda.value = 0.333333 * valor;
+
+    } else if (id == "yarda") {
+        document.lasUnidades.unid_metro.value = 0.9144 * valor;
+        document.lasUnidades.unid_pulgada.value = 36 * valor;
+        document.lasUnidades.unid_pie.value = 3 * valor;
+    }
+
+}
+
+function ConvertirGR(id) {
+        var grad, rad;
+        if(id == "grados") {
+            grad = document.getElementById("grados").value;
+            rad = (grad * Math.PI) / 180;
+
+        }else if (id == "radianes"){
+            rad = document.getElementById("radianes").value;
+            grad = (rad*180) / Math.PI;
+        }
+        document.getElementById("grados").value = grad;
+        document.getElementById("radianes").value = rad;
+
+}
