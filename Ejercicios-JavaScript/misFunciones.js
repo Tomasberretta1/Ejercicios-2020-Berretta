ef29f294d3674844729e303201c59e0171bf9a3f
Tomasberretta1 <tomi.berretta@gmail.com>
2020-05-19 16:13:18 -0300

    Se modificaron las operaciones matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -75,31 +75,31 @@ function mostrar_ocultar(valorMo) {
     }
 }
 
-
 function calcularSuma() {
     var num1,num2;
-    num1=document.getElementsByName("sum_num1")[0].value;
-    num2=document.getElementsByName("sum_num2")[0].value;
-    document.getElementsByName("sum_total")[0].value= Number(num1) + Number(num2);
+
+    num1=Number(document.getElementsByName("sum_num1")[0].value);
+    num2=Number(document.getElementsByName("sum_num2")[0].value);
+    document.getElementsByName("sum_total")[0].innerHTML= num1 + num2;
 }
 
 function calcularResta() {
     var num1,num2;
-    num1=document.getElementsByName("res_num1")[0].value;
-    num2=document.getElementsByName("res_num2")[0].value;
-    document.getElementsByName("res_total")[0].value= Number(num1) - Number(num2);
+    num1=Number(document.getElementsByName("res_num1")[0].value);
+    num2=Number(document.getElementsByName("res_num2")[0].value);
+    document.getElementsByName("res_total")[0].innerHTML= num1 - num2;
 }
 
 function calcularMul() {
     var num1,num2;
-    num1=document.getElementsByName("mul_num1")[0].value;
-    num2=document.getElementsByName("mul_num2")[0].value;
-    document.getElementsByName("mul_total")[0].value= Number(num1) * Number(num2);
+    num1=Number(document.getElementsByName("mul_num1")[0].value);
+    num2=Number(document.getElementsByName("mul_num2")[0].value);
+    document.getElementsByName("mul_total")[0].innerHTML= num1 * num2;
 }
 
 function calcularDiv() {
     var num1,num2;
     num1=document.getElementsByName("div_num1")[0].value;
     num2=document.getElementsByName("div_num2")[0].value;
-    document.getElementsByName("div_total")[0].value= Number(num1) / Number(num2);
+    document.getElementsByName("div_total")[0].innerHTML= Number(num1) / Number(num2);
 }
