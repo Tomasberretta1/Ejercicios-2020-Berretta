98b7efc3b293b52c44050707265678c4f84f14b4
Tomasberretta1 <tomi.berretta@gmail.com>
2020-05-24 17:59:53 -0300

    Mini-Paint

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -139,3 +139,28 @@ function dibujarCirCuad() {
     ctx.fillStyle = "#990a15";
     ctx.fill();
 }
+
+function dibujar(event) {
+    var canvas = document.getElementById("CanvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log(posX, posY);
+
+    canvas.onmousedown = function(){bandera=true};
+    canvas.onmouseup = function(){bandera=false};
+
+    if(bandera){
+        ctx.fillRect(posX, posY, 5,5);
+        ctx.fill;
+    }
+
+}
+
+function limpiarCanvas() {
+    var canvas = document.getElementById("CanvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+}
